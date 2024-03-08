72d1251fb6e21565c81b2767398e782047e9bd1f
Mario Medina <mariomedinalucero29@gmail.com>
2024-03-07 21:44:28 -0600

    Primeros Ejercicios

---
+++ Ejercicio2_SumaDosNumeros.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	//Suma de dos numeros enteros
+	// 8
+	int numero1,numero2,resultadosuma;
+	printf("Ingresa el primer numero ");
+	scanf ("%d",&numero1);
+	printf("Ingresa el segundo numero ");
+	scanf ("%d",&numero2);
+	resultadosuma = numero1 + numero2;
+	printf ("la suma es ");
+	printf ("%d",resultadosuma);
+	
+	count // imprimir 
+	cin // leer
+	return 0;
+}
+
